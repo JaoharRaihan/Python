7d5dc4759e21b881e24e9443f31084c6f023aade
Masum Hasan <80238860+JaoharRaihan@users.noreply.github.com>
2023-02-02 00:26:45 +0600

    a

---
+++ Operators.py
@@ -0,0 +1,33 @@
+a=3
+b=5
+print ("The values of a+b is",a+b)
+print ("The values a-b is",a-b)
+print ("The values a*b is",a*b)
+print ("The values a/b",a/b)
+
+x=20
+print (x)
+x +=3
+print (x)
+x -=5
+print (x)
+x *=3
+print (x)
+x /=4
+print (x)
+
+#comparison operators
+M=12<=2 
+N=22>=2
+h=3
+e=6
+s=h>e
+u=h<e
+o=h<=e
+print (M)
+print(N)
+print (s)
+print (o)
+print (s)
+print (u)
+print (o)
--- Variable.py
+++ Variable.py
@@ -3,6 +3,17 @@ a= "Raihan"
 # a='''Raihan'''
 b= 193014035
 c=4.00
+d= True #true flase only
+e=None #nothing
 print (a)
 print (b)
 print (c)
+print (d)
+print (e)
+
+print (type (a))
+print (type (b))
+print (type (c))
+print (type (d))
+print (type (e))
+
