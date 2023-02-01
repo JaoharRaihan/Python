d57c75fdf9bda165fede892876f9c16eb696ad3d
Masum Hasan <80238860+JaoharRaihan@users.noreply.github.com>
2023-02-02 04:58:29 +0600

    s

---
+++ chapter 3/Chapter4/storefruts.py
@@ -0,0 +1,9 @@
+f1= input("Enter fruit Number 1")
+f2= input("Enter fruit Number 2")
+f3= input("Enter fruit Number 3")
+f4= input("Enter fruit Number 4")
+f5= input("Enter fruit Number 5")
+f6= input("Enter fruit Number 6")
+f7= input("Enter fruit Number 7")
+fruitlist=[f1,f2,f3,f4,f5,f6,f7]
+print(fruitlist)
---
+++ chapter 3/Chapter4/tuplemethods.py
@@ -0,0 +1,3 @@
+t=(1,2,3,4,5)
+print(t.count(3))
+print(t.index(4))
