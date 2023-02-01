c159044175f8293843767d7c5b88ef048f0da889
Masum Hasan <80238860+JaoharRaihan@users.noreply.github.com>
2023-02-02 02:09:48 +0600

    s

--- chapter 3/String.py
+++ chapter 3/String.py
@@ -6,3 +6,9 @@ print(name[0])
 b=2
 number="12334" #01234 bi so output is 3
 print(number[b])
+# name[0]="R" ---=Does not work
+print(name[0:6]) #0=R 1=a 0123456=Raihan
+print (type(number))
+print (name[-1])
+print(name[6:0])
+print (name[0:6:2])
