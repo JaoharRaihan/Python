4d40d50da615ccb0d51fdccca40233b53911a0fa
Masum Hasan <80238860+JaoharRaihan@users.noreply.github.com>
2023-02-02 05:29:16 +0600

    s

--- chapter 3/Chapter4/Marks.py
+++ chapter 3/Chapter4/Marks.py
@@ -1,4 +1,4 @@
-R1= input("Enter Quiz Number 1")
+R1= input("Enter Quiz Number 1:")
 R2= input("Enter Quiz Number 2")
 R3= input("Enter Quiz Number 3")
 R4= input("Enter Quiz Number 4")
@@ -6,4 +6,5 @@ R5= input("Enter Quiz Number 5")
 R6= input("Enter Quiz Number 6")
 R7= input("Enter Quiz Number 7")
 Markslist=[R1,R2,R3,R4,R5,R6,R7]
+Markslist.sort()
 print(Markslist)
