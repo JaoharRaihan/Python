3c44a6934e6722ef06df51938b6c4ddc11bf6241
Masum Hasan <80238860+JaoharRaihan@users.noreply.github.com>
2023-02-02 01:14:05 +0600

    s

---
+++ Remainder.py
@@ -0,0 +1,3 @@
+a=33
+b=23
+print ("The remainder is divaided by:",a%b)
---
+++ avg.py
@@ -0,0 +1,6 @@
+a= input ("Enter first number")
+b= input ("Enter second number")
+a= int (a)
+b= int (b)
+avg= (a+b)/2
+print ("Average a and b:",avg)
