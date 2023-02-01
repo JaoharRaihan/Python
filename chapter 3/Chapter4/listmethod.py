0c8cd10788f47ac50f4469fef7a5498e72b5291c
Masum Hasan <80238860+JaoharRaihan@users.noreply.github.com>
2023-02-02 04:30:38 +0600

    s

---
+++ chapter 3/Chapter4/listmethod.py
@@ -0,0 +1,14 @@
+l1= [1,7,8,4,6,33]
+print(l1)
+# l1.sort()
+# print(l1)
+# l1.reverse()
+# print(l1)
+# l1.append(35)#end of the list
+# print(l1)
+# l1.insert(3,100)
+# print(l1)
+# l1.pop(1)
+# print(l1)
+l1.remove(6)
+print(l1)
