0d043b6dfc89c9896b875c9cb5f2edddea4c3e1d
Masum Hasan <80238860+JaoharRaihan@users.noreply.github.com>
2023-02-01 22:57:31 +0600

    s

---
+++ 01.py
@@ -0,0 +1,12 @@
+import os
+import flask
+print("Hello world")
+''' A
+B multilinecomment
+C '''
+#singlelinecomment
+print (''' Multiline
+text''')
+from playsound import playsound
+playsound('C:\\Users\\HP\\Documents\\GitHub\\Python\\ilahi.mp3')
+
---
+++ PlayAudio.py
@@ -0,0 +1,2 @@
+from playsound import playsound
+playsound('C:\\Users\\HP\\Documents\\GitHub\\Python\\ilahi.mp3')
