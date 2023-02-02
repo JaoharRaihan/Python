74340e49f78f2224181cc88ffa48b2ab31fb95dc
Masum Hasan <80238860+JaoharRaihan@users.noreply.github.com>
2023-02-02 06:04:17 +0600

    s

---
+++ chapter 3/Chapter4/chapter5/Dictonary.py
@@ -0,0 +1,9 @@
+myDict ={
+    "Fast":"In a Quick Manner",
+    "Raihan":"A coder",
+    "Marks": [1,2,3,4,5],
+    "anotherdict":{'Raihan': 'Player'}
+}
+print(myDict['Fast'])
+print(myDict['Raihan'])
+print(myDict['Marks'])
